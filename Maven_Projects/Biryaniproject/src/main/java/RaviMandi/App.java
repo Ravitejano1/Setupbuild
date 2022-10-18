1dbc66ba2602671151d476ba172b1ce88e95ae21
Ravitejano1 <traviteja1000@gmail.com>
2022-10-18 11:52:00 +0530

    Setupbuild committed successfully

---
+++ Maven_Projects/Biryaniproject/src/main/java/RaviMandi/App.java
@@ -0,0 +1,13 @@
+package RaviMandi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
